9e91dea9b4a675d3a0e073b8e63c449719fb5f6b
RosalieWessels <rosalie.wessels@yahoo.com>
2022-07-09 11:50:33 -0700

    Initial Commit

---
+++ Shared/RockPaperScissorsAvniApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsAvniApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsAvniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
